00f28f6b91226cac5a29e8b209aafb0aeb8b7b40
Adrian Chifor <me@adrianchifor.com>
2017-09-03 15:27:47 +0100

    Removed unnecessary prints

--- app/main.py
+++ app/main.py
@@ -27,14 +27,13 @@ def get_nodes():
 
 def tag_nodes():
     for node in get_nodes():
-        print node['id']
         labels = {}
         autoscaling_group = ''
         tags_ec2 = ec2.describe_instances(
             InstanceIds=[
                 node['id']
             ])['Reservations'][0]['Instances'][0]['Tags']
-        
+
         for tag in tags_ec2:
             str_clean = lambda x: x.replace(':', '.').replace('/', '-')
             key = str_clean(tag['Key'])
@@ -42,33 +41,30 @@ def tag_nodes():
             labels.update({key: value})
             if tag['Key'] == 'aws:autoscaling:groupName':
                 autoscaling_group = tag['Value']
-        
+
         if len(autoscaling_group):
             tags_asg = asg.describe_auto_scaling_groups(
                 AutoScalingGroupNames=[
                     autoscaling_group
                 ])['AutoScalingGroups'][0]['Tags']
-            
+
             for tag in tags_asg:
                 str_clean = lambda x: x.replace(':', '.').replace('/', '-')
                 key = str_clean(tag['Key'])
                 value = str_clean(tag['Value'])
                 labels.update({key: value})
-        
+
         body = {
             'kind': 'Node',
             'metadata': {
                 'labels': labels
             }
-        }   
+        }
         print labels
         add_labels = s.patch(get_url('/api/v1/nodes/' + node['name']),
                              json=body,
                              headers={'Content-Type': 'application/merge-patch+json'},
                              verify=False)
-        
-        print 'Kubernetes API response: ' + str(add_labels.status_code)
-        print '________________________________________________________'
 
 while True:
     time.sleep(60)
