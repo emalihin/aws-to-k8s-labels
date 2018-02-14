839cc1a1e2b8e4b91ba55267b091b0f0212846b0
Adrian Chifor <adrian.chifor@onfido.com>
2018-02-14 17:55:17 +0000

    Only patch k8s nodes that are missing labels from EC2 or ASG. Refactor

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.
 
 TOKEN = open('/var/run/secrets/kubernetes.io/serviceaccount/token', 'r').read()
 API_HOST = os.environ['KUBERNETES_SERVICE_HOST']
-REGION = os.environ['REGION'] # todo: get from kubernetes API
+REGION = os.environ['REGION']
 
 s = requests.Session()
 s.headers.update({'Authorization': 'Bearer ' + TOKEN})
@@ -21,14 +21,16 @@ def get_url(url):
 
 def get_nodes():
     nodes = s.get(get_url('/api/v1/nodes'), verify=False)
-    return list(map(lambda x: {'name': x['metadata']['name'], 'id': x['spec']['externalID']},
+    return list(map(lambda x: {'name': x['metadata']['name'],
+                               'id': x['spec']['externalID'],
+                               'labels': x['metadata']['labels']},
                     json.loads(nodes.content)['items']))
 
 
 def tag_nodes():
     for node in get_nodes():
-        time.sleep(0.5)
-        labels = {}
+        nodeLabels = node['labels']
+        ec2Labels = {}
         autoscaling_group = ''
         tags_ec2 = ec2.describe_instances(
             InstanceIds=[
@@ -39,7 +41,7 @@ def tag_nodes():
             str_clean = lambda x: x.replace(':', '.').replace('/', '-')
             key = str_clean(tag['Key'])
             value = str_clean(tag['Value'])
-            labels.update({key: value})
+            ec2Labels.update({key: value})
             if tag['Key'] == 'aws:autoscaling:groupName':
                 autoscaling_group = tag['Value']
 
@@ -53,22 +55,32 @@ def tag_nodes():
                 str_clean = lambda x: x.replace(':', '.').replace('/', '-')
                 key = str_clean(tag['Key'])
                 value = str_clean(tag['Value'])
-                labels.update({key: value})
+                ec2Labels.update({key: value})
 
-        body = {
-            'kind': 'Node',
-            'metadata': {
-                'labels': labels
+        # Only patch k8s node labels if they are missing something from EC2 or ASG
+        if all(item in nodeLabels.items() for item in ec2Labels.items()) is False:
+            print ec2Labels
+
+            body = {
+                'kind': 'Node',
+                'metadata': {
+                    'labels': ec2Labels
+                }
             }
-        }
-        print labels
-        add_labels = s.patch(get_url('/api/v1/nodes/' + node['name']),
-                             json=body,
-                             headers={'Content-Type': 'application/merge-patch+json'},
-                             verify=False)
+
+            s.patch(get_url('/api/v1/nodes/' + node['name']),
+                    json=body,
+                    headers={'Content-Type': 'application/merge-patch+json'},
+                    verify=False)
+
+            time.sleep(0.5)
+
+        else:
+            print node['name'] + ' has all labels.'
 
 while True:
     try:
+        time.sleep(30)
         tag_nodes()
     except Exception as e:
         print e
